b7bab1b30c53813a903984f5d2868a184170dfa2
Tom Chege <emdeechege@gmail.com>
2018-08-14 15:13:40 +0300

    forms, class added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MealComponent } from './meal/meal.component';
+import { MealFormComponent } from './meal-form/meal-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MealComponent,
+    MealFormComponent
   ],
   imports: [
     BrowserModule
